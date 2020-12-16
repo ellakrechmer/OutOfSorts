8efc70549b9f87fe5ace9a2fe1083efde0eb65c4
ellakrechmer <ekrechmer20@stuy.edu>
2020-12-15 21:31:12 -0500

    did more sucessful tests

--- SortsTester.java
+++ SortsTester.java
@@ -2,15 +2,38 @@ import java.util.*;
 public class SortsTester{
   public static void main(String [] args){
     //bubbleSort
+    //test1
     System.out.println("Bubble Sort Testing:");
     int[] a=new int[6];
-    Random rng = new Random(100);//seed of 100 is stored.
+    Random rng = new Random(1);
     for(int i = 0; i < 6; i++ ){
       a[i]=rng.nextInt() % 10;
     }
     System.out.println(toString(a));
     Sorts.bubbleSort(a);
     System.out.println(toString(a));
+
+    System.out.println();
+    //test 2
+    int[] b= new int[20];
+    rng = new Random(2);
+    for(int i = 0; i < 20; i++ ){
+      b[i]=rng.nextInt() % 100;
+    }
+    System.out.println(toString(b));
+    Sorts.bubbleSort(b);
+    System.out.println(toString(b));
+
+    System.out.println();
+    //test3
+    int[] c= new int[15];
+    rng = new Random(2);
+    for(int i = 0; i < 15; i++ ){
+      c[i]=rng.nextInt() % 1000;
+    }
+    System.out.println(toString(c));
+    Sorts.bubbleSort(c);
+    System.out.println(toString(c));
   }
   public static String toString(int[] data) {
     if (data.length==0) return "[]";
